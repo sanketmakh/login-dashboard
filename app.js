0c14f66d5a10b8954c7b255fe0ec94286999c766
unknown <vandanamakh@gmail.com>
2023-12-21 21:51:27 +0530

    adding files

--- app.js
+++ app.js
@@ -5,43 +5,21 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 // var flash=require('connect-flash');
 
-const expressSession=require("express-session");
-const passport = require('passport');
-// var routes = require('./routes/index')(passport);
-// var routes = require('./routes/index')(passport);
-var app = express();
 
-// view engine setup
+
+var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// app.use(flash());
-
-app.use(expressSession({
-  resave:false,
-  saveUninitialized:false,
-  secret:"hey"
-}))
-
-app.use(passport.initialize());
-app.use(passport.session());
-passport.serializeUser(usersRouter.serializeUser());
-passport.deserializeUser(usersRouter.deserializeUser());
-
-
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
